7456b2e533334a6eab08abd5a3c73736f9b9eb0b
Maeva <msathiu@email.com>
2025-12-29 16:04:53 -0400

    feat: dockerización completa con PostgreSQL y superusuario configurado

--- SistemaRegistro/SistemaRegistro/settings.py
+++ SistemaRegistro/SistemaRegistro/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'SistemaRegistro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'robotica_db',
+        'USER': 'msathiu',
+        'PASSWORD': 'tu_password_segura',
+        'HOST': 'db',  
+        'PORT': '5432',
     }
 }
 
